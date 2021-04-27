3eeadacf09e8111af90db44df75396d12b9c953e
Gabriel <gaplogan@gmail.com>
2021-04-27 20:06:44 -0300

    2021.04.27.19.59

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cs_1._26_AreaPerimCircunferencia
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area, perim;
+
+            Console.Write("Digite o valor do raio do círculo: ");
+            raio = double.Parse(Console.ReadLine());
+            // Garante um raio não negativo.
+            raio = Math.Abs(raio);
+
+            area = Math.PI * Math.Sqrt(raio);
+            perim = 2 * Math.PI * raio;
+
+            Console.WriteLine($"\nÁrea do círculo: {area}");
+            Console.WriteLine($"Perímetro da circunferência: {perim}");
+        }
+    }
+}
